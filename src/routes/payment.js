5e149b88bedeee06a3a2a30f38cfc9243f724be4
kowshalya <kowshalya@tigeen.ch>
2025-08-10 10:41:16 +0530

    payment webhook changes

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -1,7 +1,63 @@
 const express = require("express");
 const paymentRouter = express.Router();
-paymentRouter.get('/api/payments', (req,res)=>{
+const Payment = require("../models/Payment");
+const amqp = require("amqplib");
+const {validateWebhookSignature} = require('razorpay/dist/utils/razorpay-utils');
+/**
+ * @route   POST /api/payment/hook
+ * @desc    Razorpay Webhook Handler - Handles payment status updates from Razorpay
+ * @access  Public (should be protected by signature verification) 
+ **/
+paymentRouter.get('/api/payment/hook', async(req,res)=>{
+     try {
+       /* const signature = req.get('X-Razorpay-Signature');
+        const isWebhookValid = validateWebhookSignature(JSON.stringify(req.body), 
+        signature,
+        process.env.RAZORPAY_WEBHOOK_SECRET);
 
+        if (!isWebhookValid) {
+            return sendErrorResponse(res, 400, "webhook signature is invalid");
+        } */
+        //const paymentDetails = req.body.payload.payment.entity;
+        const paymentDetails = {
+            order_id: 'order_R3JSKZsz5rJVPv',
+            receipt: 'rec_ddf6ecc9-c61b-4606-ad8c-f774a376cee7',
+            amountPaid: '1000',
+            status: 'captured',
+            notes :{
+                firstName :'test',
+                lastName: 'test',
+                email: 'kowsi.ganeshan@gmail.com'
+            }
+        };
+       
+        const payment = await Payment.findOne({ orderId: paymentDetails.order_id});
+        payment.status = paymentDetails.status;
+        await payment.save(); 
+       
+        //publish event to RabbitMQ for cocofields to consume
+        const connection = await amqp.connect(process.env.RABBITMQ_URL);
+        const channel = await connection.createChannel();
+
+        const exchange = 'payment_events';
+        await channel.assertExchange(exchange, 'fanout', {durable: true});
+
+        const eventPayload = {
+            paymentDetails: paymentDetails,
+            paymentStatus: payment.status,
+        };
+
+        channel.publish(exchange, '', Buffer.from(JSON.stringify(eventPayload)));
+
+        await channel.close();
+        await connection.close();
+
+        res.status(200).json({ msg: "Webhook handled and event published" });
+
+    } catch (err) {
+        console.error("Error in webhook handler:", err);
+        return res.status(500).json({ msg: err.message });
+    }
 });
 
 module.exports = paymentRouter;
